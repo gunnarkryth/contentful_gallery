08720b6d6501d76f7c6c6f8bc4b5ebe51003d9e6
Chrstjan <ps5christian@hotmail.com>
2024-09-24 12:41:58 +0200

    Footer design forslag

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./assets/styles/App.scss";
+import { Footer } from "./components/Footer/Footer";
 import { Gallery } from "./components/Gallery/Gallery";
 
 function App() {
   return (
     <>
       <Gallery></Gallery>
+      <Footer />
     </>
   );
 }
