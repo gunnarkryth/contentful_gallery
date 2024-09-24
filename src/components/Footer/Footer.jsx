51c9b58a874964fe4faa83e576d18fa57d1efe94
Gunski <gunnarkryth@gmail.com>
2024-09-24 13:17:21 +0200

    Stylet footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,21 @@
 import s from "./Footer.module.scss";
 
 export const Footer = () => {
-    return (
-        <footer className={s.footerStyling}>
-            <h4>Ingen Kaj-kager kom til skade under produktionen af denne side – men deres udseende kan muligvis skræmme svage sjæle.</h4>
-            <p> © 2024 Kajkage Galleri™. Alle rettigheder forbeholdes, selv de skæve!</p>
-            <p>Gunnar</p>
-            <p>Christian</p>
-            <img src="./src/assets/images/favicon.png" />
-        </footer>
-    )
-}
+  return (
+    <footer className={s.footerStyling}>
+      <img src="./src/assets/images/favicon.png" />
+      <article>
+        <h4>
+          Ingen Kaj-kager kom til skade under produktionen af denne side – men
+          deres udseende kan muligvis skræmme svage sjæle.
+        </h4>
+        <p>
+          {" "}
+          © 2024 Kajkage Galleri™. Alle rettigheder forbeholdes, selv de skæve!
+        </p>
+        <p>Gunnar</p>
+        <p>Christian</p>
+      </article>
+    </footer>
+  );
+};
