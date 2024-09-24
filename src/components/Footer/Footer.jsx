469452fc0f319498ee1c3c600351c72ab3a19020
Chrstjan <ps5christian@hotmail.com>
2024-09-24 13:01:23 +0200

    Kaj footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import s from "./Footer.module.scss";
 export const Footer = () => {
     return (
         <footer className={s.footerStyling}>
-            <h4>Ingen Kaj-kager kom til skade under produktionen af denne side – men deres udseende kan muligvis skræmme svage sjæle. © 2024 Kajkage Galleri™. Alle rettigheder forbeholdes, selv de skæve!</h4>
+            <h4>Ingen Kaj-kager kom til skade under produktionen af denne side – men deres udseende kan muligvis skræmme svage sjæle.</h4>
+            <p> © 2024 Kajkage Galleri™. Alle rettigheder forbeholdes, selv de skæve!</p>
             <p>Gunnar</p>
             <p>Christian</p>
             <img src="./src/assets/images/favicon.png" />
